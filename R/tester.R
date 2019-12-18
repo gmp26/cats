55caf48611a35242b8917c742dd216ac01732953
gmp26 <gmp26@cam.ac.uk>
2019-12-18 14:18:57 +0000

    Added test function which returns value

---
+++ R/tester.R
@@ -0,0 +1,12 @@
+#' Tester
+#'
+#' This function allows you to express your love of cats.
+#' @param age Age at diagnosis
+#' @param size Size of tumour
+#' @keywords cats
+#' @export
+#' @examples
+#' tester()
+tester <- function(age=66, size=4){
+  return(age + size)
+}
